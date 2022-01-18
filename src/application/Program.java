565ea1404697e86bfdde16321a4e96d6b9b3ca80
Alexandra Parra <diana00suarez@gmail.com>
2022-01-18 12:25:57 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardPosition.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá");
+		Position p = new Position(3, 5);
+		System.out.println(p);
 
 	}
 
